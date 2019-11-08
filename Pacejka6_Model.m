db68ccc488fea41d229ead0425b23dbddf2167f2
saurabhvyas97 <33594953+saurabhvyas97@users.noreply.github.com>
2019-11-08 17:45:34 +0100

    First upload
    Test upload for tire data fitting.

---
+++ Pacejka6_Model.m
@@ -0,0 +1,5 @@
+function fy = Pacejka6_Model(P,x1,x2)
+% x1  = X(:,1);  %Slip
+% x2  = X(:,2);  % Fz
+D   = (P(1) + P(2)/1000.*x2).*x2;   % peak value (normalized)
+fy  = D.*sin(P(4).*atan(P(3).* (x1+P(6)))) + P(5) ;
